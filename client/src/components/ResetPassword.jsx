b2792c4101f287062d61b2038f95adaebce7bc18
Badmus <105046355+Commando-X@users.noreply.github.com>
2025-08-03 00:19:21 +0100

    Update ResetPassword.jsx

--- client/src/components/ResetPassword.jsx
+++ client/src/components/ResetPassword.jsx
@@ -36,7 +36,7 @@ const ResetPassword = () => {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/auth/reset-password', {
+      const response = await fetch('http://ripperdoc.fezzant.com:5000/api/auth/reset-password', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
